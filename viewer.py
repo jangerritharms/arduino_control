ffdaf443d6ee828d76eb2b152e32403c30c2152a
Jan Harms <jan.gerrit.harms@gmail.com>
2015-05-13 13:38:22 +0200

    Changes during test

--- viewer.py
+++ viewer.py
@@ -7,13 +7,15 @@ import numpy as np
 csv.field_size_limit(sys.maxsize)
 
 series = {}
-with open('diesdas_result.csv', 'rb') as f:
+with open('measurements/test5_10ms_0alpha_0phase_0power.csv', 'rb') as f:
     r = csv.reader(f)
     series = dict(x for x in r)
 
 print series.keys()
 counter = ast.literal_eval(series['counter'])
-fcnt = ast.literal_eval(series['thrust_force_0'])
+print counter[0]
+print counter[-1]
+fcnt = ast.literal_eval(series['lift_force_0'])
 if 'counter' in series:
     plt.plot(np.linspace(0, counter[-1]/50, len(fcnt)), \
             fcnt)
