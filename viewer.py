4d707961e3165985734c206eebbd061472290c81
Jan Harms <jan.gerrit.harms@gmail.com>
2015-05-19 09:41:08 +0200

    Added viewing of all lift curves

--- viewer.py
+++ viewer.py
@@ -7,16 +7,23 @@ import numpy as np
 csv.field_size_limit(sys.maxsize)
 
 series = {}
-with open('measurements/test5_10ms_0alpha_0phase_0power.csv', 'rb') as f:
+with open('%s'%sys.argv[1], 'rb') as f:
     r = csv.reader(f)
     series = dict(x for x in r)
 
 print series.keys()
 counter = ast.literal_eval(series['counter'])
-print counter[0]
-print counter[-1]
-fcnt = ast.literal_eval(series['lift_force_0'])
-if 'counter' in series:
-    plt.plot(np.linspace(0, counter[-1]/50, len(fcnt)), \
-            fcnt)
-    plt.show()
+curves = []
+for i in range(2, len(sys.argv)):
+    curves.append(ast.literal_eval(series[sys.argv[i]]))
+
+if all(['lift' in sys.argv[i] for i in range(2, len(sys.argv))]):
+    resultant = [sum(row[i] for row in curves) for i in range(len(curves[0]))]
+
+plt.plot(np.linspace(0, (counter[-1]-counter[0])/50, len(resultant)), resultant, label='resultant')
+plt.legend()
+for curve in curves:
+    plt.plot(np.linspace(0, (counter[-1]-counter[0])/50, len(curve)), \
+            curve)
+
+plt.show()
